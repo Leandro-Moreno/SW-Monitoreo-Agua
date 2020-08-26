5f67e3f6f7bbfd61ef27de5e43d2391815c624fc
Leandro Moreno <123857@gmail.com>
2020-08-25 21:09:36 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register', 'Api\AuthController@register');
-Route::post('/login', 'Api\AuthController@login');
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
 
 Route::resource('/registro', 'API\RegistroController');
 Route::resource('/region', 'API\RegionController');
