7195fc9a2df9effb283f88ad747bfa96c324a345
Leandro Moreno <123857@gmail.com>
2020-09-04 09:53:14 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,9 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (Request::server('HTTP_X_FORWARDED_PROTO') == 'https')
-      {
-         URL::forceScheme('https');
-      }
+        if($this->app->environment('production')) {
+              \URL::forceScheme('https');
+          }
     }
 }
