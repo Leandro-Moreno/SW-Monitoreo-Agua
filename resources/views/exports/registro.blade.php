821b726927bd58bb0c8ef6dc71b91b72e4fc7e01
Leandro Moreno <123857@gmail.com>
2020-11-30 21:14:41 -0500

    Update registro.blade.php

--- resources/views/exports/registro.blade.php
+++ resources/views/exports/registro.blade.php
@@ -54,7 +54,7 @@
     <td>{{$registro->od}}</td>
     <td>{{$registro->ph}}</td>
     <td>
-      {{$registro->region->nombre}}
+      {{$registro->region->nombre?$registro->region->nombre:""}}
     </td>
     <td>{{$registro->created_at}}</td>
     <td>{{$registro->transferencia->estado}}</td>
